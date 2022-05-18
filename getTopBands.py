8ab140b02f6b9e81d0d6dce1e00ff54bf1174986
ekrell <ekrell@islander.tamucc.edu>
2022-05-18 14:01:31 -0500

    contours for the plots

--- getTopBands.py
+++ getTopBands.py
@@ -4,13 +4,11 @@ import numpy as np
 import pandas as pd
 import pickle
 from optparse import OptionParser
+import matplotlib
 from matplotlib import colors
 import matplotlib.pyplot as plt
 import copy
 import shap
-
-colorRed = (250./255, 150./255, 150./255)
-colorBlue = (150./255, 150./255, 250./255)
 
 def main():
     parser = OptionParser()
@@ -43,16 +41,6 @@ def main():
                       help="Only calculate top bands, no plotting",
                       default=False, action="store_true")
 
-    # SHAP values (optional, secondary)
-    parser.add_option(      "--pickled_shap_2",
-                      help="Path to pickled SHAP values (optional, comparison SHAP values).")
-    parser.add_option(      "--class_index_2",
-                      help = "Index of class to use.",
-                      default=0, type="int")
-    parser.add_option(      "--class_name_2",
-                      help="Name of optional comparison SHAP values (see option `--pickled_shap_2`.",
-                      default = "default-class-name-2")
-
     options, args = parser.parse_args()
 
     # SHAP values (primary)
@@ -82,11 +70,6 @@ def main():
             bandDescs = f.read().splitlines()
     noPlot = options.no_plot
 
-    # SHAP values (optional, secondary)
-    infile2 = options.pickled_shap_2
-    classIdx2 = options.class_index_2
-    className2 = options.class_name_2
-
     print("")
     print("Get top N SHAP bands (N = {})".format(numBands))
     print("Class index: {}".format(classIdx))
@@ -108,19 +91,14 @@ def main():
     print("Number of prediction classes saved: {}".format(nClasses))
     print("")
 
-    if infile2 is not None:
-        print("  Using secondary SHAP values")
-        print("  Class index: {}".format(classIdx2))
-        print("        name: {}".format(className2))
-        print("  SHAP pickle file: {}".format(infile2))
+    fig = plt.figure(figsize=(14,10))
+    gs = fig.add_gridspec(2,3)
 
-    if infile2 is None:
-        fig = plt.figure(figsize=(12,10))
-        gs = fig.add_gridspec(2,3)
-    else:
-        fig = plt.figure(figsize=(14,10))
-        gs = fig.add_gridspec(4,3)
+    font = {'family' : 'normal',
+            'weight' : 'normal',
+            'size'   : 14}
 
+    matplotlib.rc('font', **font)
 
     #############
     # Functions #
@@ -178,10 +156,6 @@ def main():
         return bandDescs
 
 
-    ######################
-    # Primary: top bands #
-    ######################
-
     # Extract only the shap values
     shap_values = list(shap_values.values)
 
@@ -199,10 +173,7 @@ def main():
     posCounts = np.array([np.count_nonzero(np.array(shapTopBandsPos) == b) for b in range(bands)])
     negCounts = np.array([np.count_nonzero(np.array(shapTopBandsNeg) == b) for b in range(bands)])
 
-    ##########################
-    # Primary: make CSV file #
-    ##########################
-
+    # Make CSV file
     shapOrderedBands = orderBandsByFreq(shapBands, bands)
     shapOrderedBandDescs = orderBandDescs(shapOrderedBands, bandDescs)
 
@@ -216,9 +187,6 @@ def main():
     df = pd.DataFrame(dfDict)
 
     if noPlot == False:
-        #################
-        # Primary: plot #
-        #################
 
         ax0 = fig.add_subplot(gs[0:2, 0])
         ax1A = fig.add_subplot(gs[0, 1])
@@ -229,189 +197,99 @@ def main():
         maxCount = max(np.max(shapAbsTopCounts), np.max(shapAbsBottomCounts))
 
         # Plot count of band occurances in top N bands:   SHAP magnitude for top and bottom occurances
-        mainColor = (105.0/255.0, 225.0/255.0, 105.0/255.0)
-        ax0.bar(x=range(bands), height=shapAbsTopCounts, width=4, color=mainColor)
+        bandLabels = range(bands)
+        cmap = matplotlib.cm.get_cmap('Set2').colors
+        cmapLen = len(cmap)
+
+        if len(groups) == 4:
+            cmap = [
+                   ("#6b7f9f"), ("#b19172"), ("#628254"), ("#ab7171"), ("#877d9f"), ("#a4c2f4"),
+            ]
+
+        a = 0
+        groups_ = groups + [len(shapAbsTopCounts)]
+        for i, g in enumerate(groups_):
+            b = g
+            rgba = cmap[i % cmapLen]
+            ax0.bar(x=bandLabels[a:b], height = shapAbsTopCounts[a:b], width=4, color=rgba)
+            ax1A.bar(x=bandLabels[a:b], height = shapAbsTopCounts[a:b], width=4, color=rgba)
+            ax1B.bar(x=bandLabels[a:b], height = shapAbsBottomCounts[a:b], width=4, color=rgba)
+            ax2A.bar(x=bandLabels[a:b], height = posCounts[a:b], width=4, color=rgba)
+            ax2B.bar(x=bandLabels[a:b], height = negCounts[a:b], width=4, color=rgba)
+            a = b
+
         ax0.set_xlim(-1, bands+1)
         ax0.set_ylim(0, maxCount)
         ax0.set_title(className)
         ax0.set_xlabel("Band")
-        ax0.set_ylabel("Occurances in top {}".format(numBands))
+        ax0.set_ylabel("Occurrences in top {}".format(numBands))
         for g in groups:
             ax0.axvline(g, color="black", linestyle="dotted", lw=3)
 
-        ax1A.bar(x=range(bands), height=shapAbsTopCounts, width=4, color=mainColor)
         ax1A.set_xlim(-1, bands+1)
         ax1A.set_ylim(0, maxCount)
-        ax1A.set_title("Occurances in top/bottom {} bands".format(numBands))
+        ax1A.set_title("Top {} bands".format(numBands))
         ax1A.set_xlabel(None)
-        ax1A.set_ylabel("Top bands")
+        ax1A.set_ylabel("Occurrences")
         ax1A.set_xticks([])
-        for g in groups:
+        a = 0
+        for i, g in enumerate(groups_):
+            b = float(g)
             ax1A.axvline(g, color="black", linestyle="dotted", lw=3)
+            ax1A.text((a + 0.5 * (b - a) - 10) / groups_[-1], -0.05, "G" + str(i + 1), transform=ax1A.transAxes)
+            a = b
 
-        ax1B.bar(x=range(bands), height=shapAbsBottomCounts, width=4, color="tab:gray")
         ax1B.set_xlim(-1, bands+1)
         ax1B.set_ylim(0, maxCount)
-        ax1B.set_xlabel("Band")
-        ax1B.set_ylabel("Bottom bands")
+        ax1B.set_ylabel("Occurrences")
+        ax1B.set_title("Bottom {} bands".format(numBands), y=-0.075)
+        ax1B.set_xticks([])
         for g in groups:
             ax1B.axvline(g, color="black", linestyle="dotted", lw=3)
+        a = 0
         ax1B.invert_yaxis()
+        for i, g in enumerate(groups_):
+            b = float(g)
+            ax1B.axvline(g, color="black", linestyle="dotted", lw=3)
+            ax1B.text((a + 0.5 * (b - a) - 10) / groups[-1], 1.01, "G" + str(i + 1), transform=ax1B.transAxes)
+            a = b
 
-        # Plot count of band occurances in top N bands:   POS and NEG SHAP values
-        ax2A.bar(x=range(bands), height=posCounts, width=4, color=colorRed)
         ax2A.set_xlim(-1, bands+1)
         ax2A.set_ylim(0, maxCount)
-        ax2A.set_title("Occurances in top {} bands".format(numBands))
+        ax2A.set_title("Top positive {} bands".format(numBands))
         ax2A.set_xlabel(None)
-        ax2A.set_ylabel("Top positive")
+        ax2A.set_ylabel("Occurrences")
         ax2A.set_xticks([])
         for g in groups:
             ax2A.axvline(g, color="black", linestyle="dotted", lw=3)
-        ax2B.bar(x=range(bands), height=negCounts, width=4, color=colorBlue)
+        a = 0
+        for i, g in enumerate(groups_):
+            b = float(g)
+            ax2A.axvline(g, color="black", linestyle="dotted", lw=3)
+            ax2A.text((a + 0.5 * (b - a) - 10) / groups_[-1], -0.05, "G" + str(i + 1), transform=ax2A.transAxes)
+            a = b
+
         ax2B.set_xlim(-1, bands+1)
         ax2B.set_ylim(0, maxCount)
-        ax2B.set_xlabel("Band")
-        ax2B.set_ylabel("Top negative")
+        ax2B.set_title("Top negative {} bands".format(numBands), y=-0.075)
+        ax2B.set_ylabel("Occurrences")
+        ax2B.set_xticks([])
         for g in groups:
             ax2B.axvline(g, color="black", linestyle="dotted", lw=3)
         ax2B.invert_yaxis()
-
-
-    #########################################
-    # [Optional] Add secondary SHAP results #
-    #########################################
-    if infile2 is not None:
-
-        ######################
-        # Secondary: top bands #
-        ######################
-
-        # Extract only the shap values
-        shap_values = pickle.load(open(infile2, "rb"))
-        nInstances, rows, cols, bands, nClasses = shap_values.values.shape
-        shap_values = list(shap_values.values)
-
-        # SHAP absolute values
-        shapAbsSorts, shapAbsBands = sortShap(np.absolute(shap_values))
-        shapAbsTopCounts, shapAbsTopSums, shapAbsTopBands, \
-            shapAbsBottomCounts, shapAbsBottomSums, shapAbsBottomBands = countBands(shapAbsSorts, shapAbsBands, bands, numBands)
-
-        # SHAP values
-        shapSorts, shapBands = sortShap(shap_values)
-        shapTopCounts, shapTopSums, shapTopBands, \
-            shapBottomCounts, shapBottomSums, shapBottomBands = countBands(shapSorts, shapBands, bands, numBands)
-        shapTopBandsPos, shapTopBandsNeg = filterBandsBySign(shapTopBands, shapTopSums, shapBottomBands, shapBottomSums, nInstances)
-
-        posCounts = np.array([np.count_nonzero(np.array(shapTopBandsPos) == b) for b in range(bands)])
-        negCounts = np.array([np.count_nonzero(np.array(shapTopBandsNeg) == b) for b in range(bands)])
-
-        ##########################
-        # Primary: make CSV file #
-        ##########################
-
-        shapOrderedBands = orderBandsByFreq(shapBands, bands)
-        shapOrderedBandDescs = orderBandDescs(shapOrderedBands, bandDescs)
-
-        shapAbsOrderedBands = orderBandsByFreq(shapAbsBands, bands)
-        shapAbsOrderedBandDescs = orderBandDescs(shapAbsOrderedBands, bandDescs)
-
-        dfDict = {'{}_shap_band'.format(className2) : shapOrderedBands,
-              '{}_shap_desc'.format(className2) : shapOrderedBandDescs,
-              '{}_shap_abs_band'.format(className2) : shapAbsOrderedBands,
-              '{}_shap_abs_desc'.format(className2) : shapAbsOrderedBandDescs}
-        df2 = pd.DataFrame(dfDict)
-
-        # Combine primary and secondary dataframes
-        df = pd.concat((df, df2), axis=1)
-
-        if noPlot == False:
-            #################
-            # Primary: plot #
-            #################
-
-            ax02 = fig.add_subplot(gs[2:, 0])
-            ax1A2 = fig.add_subplot(gs[2, 1])
-            ax1B2 = fig.add_subplot(gs[3, 1])
-            ax2A2 = fig.add_subplot(gs[2, 2])
-            ax2B2 = fig.add_subplot(gs[3, 2])
-
-            ax0.set_title("")
-            ax0.set_ylabel("{}: occurances in top {}".format(className, numBands))
-
-            # Plot count of band occurances in top N bands:   SHAP magnitude for top and bottom occurances
-            ax02.bar(x=range(bands), height=shapAbsTopCounts, width=4, color=mainColor)
-            ax02.set_xlim(-1, bands+1)
-            ax02.set_ylim(0, maxCount)
-            ax02.set_xlabel("Band")
-            ax02.set_ylabel("{}: occurances in top {}".format(className2, numBands))
-            for g in groups:
-                ax02.axvline(g, color="black", linestyle="dotted", lw=3)
-            ax02.invert_yaxis()
-
-            ax1A2.bar(x=range(bands), height=shapAbsTopCounts, width=4, color=mainColor)
-            ax1A2.set_xlim(-1, bands+1)
-            ax1A2.set_ylim(0, maxCount)
-            ax1A2.set_title("Occurances in top/bottom {} bands".format(numBands))
-            ax1A2.set_xlabel(None)
-            ax1A2.set_ylabel("Top bands")
-            ax1A2.set_xticks([])
-            for g in groups:
-                ax1A2.axvline(g, color="black", linestyle="dotted", lw=3)
-
-            ax1B2.bar(x=range(bands), height=shapAbsBottomCounts, width=4, color="tab:gray")
-            ax1B2.set_xlim(-1, bands+1)
-            ax1B2.set_ylim(0, maxCount)
-            ax1B2.set_xlabel("Band")
-            ax1B2.set_ylabel("Bottom bands")
-            for g in groups:
-                ax1B2.axvline(g, color="black", linestyle="dotted", lw=3)
-            ax1B2.invert_yaxis()
-
-            # Plot count of band occurances in top N bands:   POS and NEG SHAP values
-            ax2A2.bar(x=range(bands), height=posCounts, width=4, color=colorRed)
-            ax2A2.set_xlim(-1, bands+1)
-            ax2A2.set_ylim(0, maxCount)
-            ax2A2.set_title("Occurances in top {} bands".format(numBands))
-            ax2A2.set_xlabel(None)
-            ax2A2.set_ylabel("Top positive")
-            ax2A2.set_xticks([])
-            for g in groups:
-                ax2A2.axvline(g, color="black", linestyle="dotted", lw=3)
-            ax2B2.bar(x=range(bands), height=negCounts, width=4, color=colorBlue)
-            ax2B2.set_xlim(-1, bands+1)
-            ax2B2.set_ylim(0, maxCount)
-            ax2B2.set_xlabel("Band")
-            ax2B2.set_ylabel("Top negative")
-            for g in groups:
-                ax2B2.axvline(g, color="black", linestyle="dotted", lw=3)
-            ax2B2.invert_yaxis()
-
-
-            def adjustax(ax, a, b):
-                pos = ax.get_position()
-                pnts = pos.get_points()
-                pnts[0][1] = pnts[0][1] + a
-                pnts[1][1] = pnts[1][1] + b
-                pos.set_points(pnts)
-                ax.set_position(pos)
-
-
-            plt.tight_layout()
-
-            adjustax(ax0, -0.035, -0.035)
-            adjustax(ax02, 0.035, 0.035)
-            adjustax(ax1B, 0.07, 0.07)
-            adjustax(ax2B, 0.07, 0.07)
-            adjustax(ax1A2, -0.07, -0.07)
-            adjustax(ax2A2, -0.07, -0.07)
-
+        a = 0
+        for i, g in enumerate(groups_):
+            b = float(g)
+            ax2B.axvline(g, color="black", linestyle="dotted", lw=3)
+            ax2B.text((a + 0.5 * (b - a) - 10) / groups[-1], 1.01, "G" + str(i + 1), transform=ax2B.transAxes)
+            a = b
 
     # Write CSV
     if outfile is not None:
         df.to_csv(outfile, index=False)
 
     # Save plot
+    plt.tight_layout()
     plt.savefig(plotfile)
 
 
--- plot-2D.py
+++ plot-2D.py
@@ -33,7 +33,9 @@ def main():
                       help="Number of top bands to plot. Ignores '-b' option.",
                       type = "int")
     parser.add_option(      "--band_names",
-                      help="Comma-delmited list of band names for plot titles")
+                      help="Comma-delimited list of band names for plot titles")
+    parser.add_option(      "--marker",
+                      help="Comma-delimited row,col,size for a plot marker")
     parser.add_option("-o", "--output_file",
                       help="Path to save visualization image.")
 
@@ -43,6 +45,10 @@ def main():
     outfile = options.output_file
     instanceIdx = options.instance_index
     classIdx = options.class_index
+
+    marker = options.marker
+    if marker is not None:
+        marker = np.array(marker.split(",")).astype(float)
 
     # Load data from pickle
     shap_values, data_values, base_values, class_labels = loadPickle(infile, instanceIdx, classIdx)
@@ -80,10 +86,14 @@ def main():
     if nTop:
         bandNames = [bandNames[i] for i in sortIdx]
 
-    fig, ax = plt.subplots(len(bands), 3, squeeze=False, figsize=(8, 4 * len(bands)))
+    fig, ax = plt.subplots(len(bands), 5, squeeze=False, figsize=(12, 4 * len(bands)))
 
     abs_vals = np.abs(shap_values.sum(-1))
     max_val = np.nanpercentile(abs_vals, 99.9)
+
+
+    plt.rcParams["axes.edgecolor"] = "black"
+    plt.rcParams["axes.linewidth"] = 2.50
 
     for i, band in enumerate(bands):
 
@@ -96,17 +106,37 @@ def main():
         ax[i][0].set_title(bandNames[i])
         ax[i][0].imshow(data_b, cmap="gray")
         ax[i][0].invert_yaxis()
-        ax[i][0].axis("off")
         ax[i][1].set_title("SHAP: scaled by all")
         #ax[i][1].imshow(data_b, cmap="gray", alpha=0.25)
         ax[i][1].imshow(shap_b, cmap="seismic", vmin=-max_val, vmax=max_val)
         ax[i][1].invert_yaxis()
-        ax[i][1].axis("off")
-        ax[i][2].set_title("SHAP: scaled by self")
-        #ax[i][2].imshow(data_b, cmap="gray", alpha=0.25)
-        ax[i][2].imshow(shap_b, cmap=colors.red_transparent_blue, vmin=-local_max_val, vmax=local_max_val)
+        ax[i][2].set_title("")
+        ax[i][2].imshow(shap_b, cmap=colors.red_transparent_blue, vmin=-max_val, vmax=max_val)
+        contours = ax[i][2].contour(data_b, colors='black', )
+        #ax[i][2].clabel(contours, inline=True, fontsize=8)
         ax[i][2].invert_yaxis()
-        ax[i][2].axis("off")
+        ax[i][3].set_title("SHAP: scaled by self")
+        #ax[i][3].imshow(data_b, cmap="gray", alpha=0.25)
+        ax[i][3].imshow(shap_b, cmap=colors.red_transparent_blue, vmin=-local_max_val, vmax=local_max_val)
+        ax[i][3].invert_yaxis()
+        ax[i][4].set_title("")
+        ax[i][4].imshow(shap_b, cmap=colors.red_transparent_blue, vmin=-local_max_val, vmax=local_max_val)
+        contours = ax[i][4].contour(data_b, colors='black', alpha=0.5)
+        #ax[i][4].clabel(contours, inline=True, fontsize=8)
+        ax[i][4].invert_yaxis()
+
+
+        for j in range(5):
+            ax[i][j].set_xticks([])
+            ax[i][j].set_xticks([], minor=True)
+            ax[i][j].set_yticks([])
+            ax[i][j].set_yticks([], minor=True)
+
+
+        if marker is not None:
+            mcolor="#b0e88b"
+            for j in range(5):
+                ax[i][j].scatter(marker[1], marker[0], marker="*", s=marker[2], c=mcolor, alpha=0.9, edgecolors="#40612b", zorder=100)
 
     if outfile is not None:
         plt.savefig(outfile, bbox_inches='tight')
